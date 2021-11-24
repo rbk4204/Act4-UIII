fc98cdfe529cec7e8e3ce3290e2a3231736bdb2e
Rebe <91562738+rbk4204@users.noreply.github.com>
2021-11-24 02:40:15 -0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$IDempleado=$_GET['IDempleado'];
+$sql="DELETE FROM 'empleado'  WHERE 'IDempleado'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: empleado.php");
+    }
+?>
